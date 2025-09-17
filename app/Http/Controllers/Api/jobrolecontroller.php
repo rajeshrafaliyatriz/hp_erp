b4065e975bd41e6337d00311457845aa4f19439b
Sonika <riddhiwork1212>
2025-09-17 10:26:39 +0530

    create 2 api

--- app/Http/Controllers/Api/jobrolecontroller.php
+++ app/Http/Controllers/Api/jobrolecontroller.php
@@ -30,10 +30,71 @@ class jobrolecontroller extends Controller
         ->groupBy('track')
         ->get();
 
-    return response()->json([
-        'department_id' => $id,
-        'job_roles' => $jobRoles
-    ], 200);
+    return response()->json(['department_id' => $id,'job_roles' => $jobRoles], 200);
 }
 
+
+     public function skills(Request $request, $id)
+    {
+
+        $token = $request->input('token');
+        if (!$token) {
+            return response()->json(['message' => 'Token not provided'], 401);
+        }
+
+        $accessToken = PersonalAccessToken::findToken($token);
+        if (!$accessToken) {
+            return response()->json(['message' => 'Invalid token'], 401);
+        }
+
+        // $subInstituteId = $request->query('sub_institute_id'); 
+        $skills = DB::table('s_user_skill_jobrole as a')
+                ->join('s_users_skills as b', function ($join) use ($request){
+                    $join->on('b.title', '=', 'a.skill')
+                        ->where('b.sub_institute_id', $request->sub_institute_id)
+                        ->whereNull('b.deleted_at');
+                        })
+                        ->where('a.sub_institute_id', $request->sub_institute_id)
+                        ->whereNull('a.deleted_at')
+                        ->where('a.jobrole', $id)
+                        ->groupBy('a.skill')
+                        ->get();
+            
+
+         return response()->json(['status' => 1,'skills' => $skills]);
+
+    }
+
+
+    public function searchskills(Request $request)
+    {
+
+        
+        $token = $request->input('token');
+        if (!$token) {
+            return response()->json(['message' => 'Token not provided'], 401);
+        }
+
+        $accessToken = PersonalAccessToken::findToken($token);
+        if (!$accessToken) {
+            return response()->json(['message' => 'Invalid token'], 401);
+        }
+        $skill = $request->input('query');
+        $skills = DB::table('s_user_skill_jobrole as a')
+                 ->join('s_users_skills as b', function($join) use ($request)
+                 {
+                    $join->on('b.title', '=', 'a.skill')
+                        ->where('b.sub_institute_id',$request->sub_institute_id)
+                        ->whereNull('b.deleted_at');
+                            })
+                            ->where('a.sub_institute_id',$request->sub_institute_id)
+                            ->whereNull('a.deleted_at')
+                            ->where('a.skill', 'LIKE', "%{$skill}%")
+                            ->groupBy('a.skill')
+                            ->get();
+
+
+        return response()->json(['status' => 1,'skills' => $skills]);
+    
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\IndustryController;
 use App\Http\Controllers\Api\jobrolecontroller;
 
+Route::get('skills/search', [jobrolecontroller::class, 'searchskills']);
+
+Route::get('jobrole/{id}/skills', [jobrolecontroller::class, 'skills']);
+
 Route::get('/department/{id}/jobroles', [jobrolecontroller::class, 'getJobRolesByDepartment']);
 
 
