a420e22ac16fc9c81c7a1a4da8c6b93aa8004c1d
Sonika <riddhiwork1212>
2025-10-10 10:58:55 +0530

    job-posting and job-application API for talent mangement

---
+++ app/Models/talent/talent_jobposting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\talent;
+
+use Illuminate\Database\Eloquent\Model;
+use app\Http\Controllers\talent\talent_jobpostingcontroller;
+
+
+class talent_jobposting extends Model
+{
+    protected $table = 'talent_job_postings';
+}
