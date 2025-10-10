2d95a109a241ec29b4a9a66dd558df2e5e373d82
sonikapansuriya45h-create <sonikapansuriya45h@gmail.com>
2025-10-10 11:04:36 +0530

    Merge pull request #89 from rajeshrafaliyatriz/sonikaDev
    job-posting and job-application API for talent mangement

---
+++ app/Models/talent/talent_jobapplication.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\talent;
+
+use Illuminate\Database\Eloquent\Model;
+use app\Http\Controllers\talent\talent_jobapplicationcontroller;
+
+class talent_jobapplication extends Model
+{
+
+    protected $table = 'talent_job_applications';
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,13 @@ use App\Http\Controllers\libraries\jobroletaskcontroller;
 use App\Http\Controllers\libraries\jobroleskillcontroller;
 use App\Http\Controllers\HRMS\HrmsController;
 
-use App\Http\Controllers\talent_management\talentmanagement_jobpostingController;
-use App\Http\Controllers\talent_management\talentmanagement_jobapplicationController;
+use App\Http\Controllers\talent\talent_jobpostingcontroller;
+use App\Http\Controllers\talent\talent_jobapplicationcontroller;
 
+Route::resource('job-applications', talent_jobapplicationcontroller::class);
 
-Route::Resource('job-applications', talentmanagement_jobapplicationController::class);
+Route::resource('job-postings', talent_jobpostingcontroller::class);
 
-
-Route::Resource('talents', talentmanagement_jobpostingController::class);
 Route::post('designation_leave', [HrmsLeaveController::class, 'store']);
 
 Route::post('/jobrole-skill/store', [jobroleskillcontroller::class, 'storeSkill']);
