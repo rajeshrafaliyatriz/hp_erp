da8121b256e24dc0c0cae32fd780f5f34a32bc28
Rajesh <rajesh.rafaliya@triz.co.in>
2025-11-06 16:26:41 +0530

    HP_ERP: 06-11-2025 by Rajesh

--- app/Models/talent/talent_jobapplication.php
+++ app/Models/talent/talent_jobapplication.php
@@ -7,8 +7,27 @@ use app\Http\Controllers\talent\talent_jobapplicationcontroller;
 
 class talent_jobapplication extends Model
 {
-
-    protected $table = 'talent_job_applications';
+	protected $table = 'talent_job_applications';
+    protected $fillable = [
+        'job_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'mobile',
+        'current_location',
+        'employment_type',
+        'experience',
+        'education',
+        'expected_salary',
+        'skills',
+        'certifications',
+        'resume_path',
+        'applied_date',
+        'status',
+        'sub_institute_id',
+        'created_by',
+    ];
 }
 
 
