0eb512e522ed17c88af193477931a4b38de53d87
Rajesh <rajesh.rafaliya@triz.co.in>
2025-11-12 15:34:00 +0530

    HP: 12-11-2025 by Rajesh

--- routes/api.php
+++ routes/api.php
@@ -7,42 +7,25 @@ use App\Http\Controllers\libraries\jobroletexonomycontroller;
 use App\Http\Controllers\libraries\jobroletaskcontroller;
 use App\Http\Controllers\libraries\jobroleskillcontroller;
 use App\Http\Controllers\HRMS\HrmsController;
-
+use App\Http\Controllers\Api\CompetencyDashboardController;
 use App\Http\Controllers\talent\talent_jobpostingcontroller;
 use App\Http\Controllers\talent\talent_jobapplicationcontroller;
 use App\Http\Controllers\talent\talent_interviewschedulescontroller;
 
 Route::resource('interview-schedules', talent_interviewschedulescontroller::class);
-
 Route::resource('job-applications', talent_jobapplicationcontroller::class);
 Route::post('job-applications/{id}/status', [talent_jobapplicationcontroller::class, 'updateStatus']);
 Route::get('job-applications/candidate/{candidate_id}', [talent_jobapplicationcontroller::class, 'getCandidateApplications']);
-
-
 Route::resource('job-postings', talent_jobpostingcontroller::class);
-
 Route::post('designation_leave', [HrmsLeaveController::class, 'store']);
-
 Route::post('/jobrole-skill/store', [jobroleskillcontroller::class, 'storeSkill']);
-
-
 Route::resource('job-role-tasks', jobroletaskcontroller::class);
-
-
 Route::resource('jobroletexonomies', jobroletexonomycontroller::class);
-
 Route::resource('skills', skillcontroller::class);
-
 Route::get('skills/search', [jobrolecontroller::class, 'searchskills']);
-
 Route::get('jobrole/{id}/skills', [jobrolecontroller::class, 'skills']);
-
 Route::get('/department/{id}/jobroles', [jobrolecontroller::class, 'getJobRolesByDepartment']);
-
-
 Route::get('/industry/{id}/departments', [IndustryController::class, 'departments']);
-
-
 Route::get('/industries', [IndustryController::class, 'index']);
-
+Route::get('/competency-dashboard', [CompetencyDashboardController::class, 'index']);
 ?>
